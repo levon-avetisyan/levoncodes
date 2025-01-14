8923844d785be22f1f155ee36a9c65f6a2bb8bef
Levon <levon.s.avetisyan@gmail.com>
2025-01-14 16:32:53 +0400

    8

--- sendmail.php
+++ sendmail.php
@@ -7,7 +7,7 @@ require '/home/levoncodes/public_html/vendor/phpmailer/phpmailer/src/Exception.p
 use PHPMailer\PHPMailer\PHPMailer;
 
 // Load environment variables
-require '/home/levoncodes/env.php';
+require '/home/levoncodes/env.php'; // Updated path to env.php
 
 // Validate and sanitize input
 $name = filter_var($_POST['name'] ?? '', FILTER_SANITIZE_STRING);
@@ -23,46 +23,23 @@ $mail = new PHPMailer();
 try {
     $mail->isSMTP();
     $mail->Mailer = "smtp";
-    $mail->SMTPDebug = 0; // Use 1 or 2 for debugging
+    $mail->SMTPDebug = 2; // Debug level (0 = no output, 1 = commands, 2 = full)
     $mail->SMTPAuth = true;
-    $mail->SMTPSecure = getenv('SMTP_SECURE');
-    $mail->Port = getenv('SMTP_PORT');
-    $mail->Host = getenv('SMTP_HOST');
+    $mail->Timeout = 30; // Connection timeout
+    $mail->SMTPSecure = getenv('SMTP_SECURE') ?: 'tls';
+    $mail->Port = getenv('SMTP_PORT') ?: 465;
+    $mail->Host = getenv('SMTP_HOST') ?: 'mail.levon.codes';
     $mail->Username = getenv('SMTP_USERNAME');
     $mail->Password = getenv('SMTP_PASSWORD');
     $mail->isHTML(true);
     $mail->CharSet = 'UTF-8';
 
-    // Set email details
     $mail->setFrom(getenv('SMTP_USERNAME'), 'Levon Codes');
     $mail->addAddress('levon.s.avetisyan@gmail.com');
     $mail->Subject = 'Message from levon.codes contact form';
     $mail->Body = "Message: {$message}<br>Name: {$name}<br>Email: {$email}";
     $mail->AltBody = "Message: {$message}\nName: {$name}\nEmail: {$email}";
 
-    // Handle file attachments
-    if (!empty($_FILES['userfile']['tmp_name'])) {
-        $finfo = finfo_open(FILEINFO_MIME_TYPE); // Open file info resource
-        for ($ct = 0; $ct < count($_FILES['userfile']['tmp_name']); $ct++) {
-            $tmpName = $_FILES['userfile']['tmp_name'][$ct];
-            $fileName = basename($_FILES['userfile']['name'][$ct]);
-
-            // Validate file type and size
-            $fileMimeType = finfo_file($finfo, $tmpName); // Get MIME type
-            $allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
-            $maxFileSize = 2 * 1024 * 1024; // 2 MB
-
-            if (in_array($fileMimeType, $allowedTypes) && filesize($tmpName) <= $maxFileSize) {
-                $uploadFile = tempnam(sys_get_temp_dir(), hash('sha256', $fileName));
-                if (move_uploaded_file($tmpName, $uploadFile)) {
-                    $mail->addAttachment($uploadFile, $fileName);
-                }
-            }
-        }
-        finfo_close($finfo); // Close file info resource
-    }
-
-    // Send email
     if (!$mail->send()) {
         throw new Exception('Mail not sent: ' . $mail->ErrorInfo);
     }
